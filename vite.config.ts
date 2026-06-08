692d2b8f50469e285d19b67bff54dfe256cf5dd4
KellinQuinn01 <47975483+tsunaalfonso@users.noreply.github.com>
2026-06-09 00:14:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackStart(),
+    react(),
+    tsconfigPaths(),
+  ],
 })
